6b8e25053f938fc7f928f2c20cb5d708e7c5d3d0
RozhakXd <rozaqsapa@gmail.com>
2025-03-13 01:00:00 +0700

    Final Release

---
+++ Run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+_A='bold light_steel_blue1'
+try:import json,datetime,random,os,time,jwt,emoji,shutil,re,sys,datetime,requests,string,urllib.parse;from requests.exceptions import RequestException;from rich.console import Console;from platform import platform;from rich.panel import Panel;from rich.tree import Tree;from rich.columns import Columns;from concurrent.futures import ThreadPoolExecutor
+except ModuleNotFoundError as e:import os,sys,urllib.parse;os.system(f"xdg-open https://wa.me/6283847921480?text=SKY%20HACK%20%3A%20{urllib.parse.quote(str(e))}%21");sys.exit(0)
+os.system('cls'if os.name=='nt'else'clear')
+Console(style=_A,width=65).print(Panel('[bold red]●[bold yellow] ●[bold green] ●[bold white]\n[bold blue]      ____  _          _   _            _        _  _   \n     / ___|| | ___   _| | | | __ _  ___| | __  _| || |_ \n     \\___ \\| |/ / | | | |_| |/ _` |/ __| |/ / |_  ..  _|\n      ___) |   <| |_| |  _  | (_| | (__|   <  |_      _|\n     |____/|_|\\_\\\\__, |_| |_|\\__,_|\\___|_|\\_\\   |_||_|  \n                 |___/                                  \n[bold red]   ||[bold white] Version:[bold green] 1.0.0[bold red] ||[bold white] Author:[bold green] Rozhak[bold red] ||[bold white] Type:[bold green] Premium[bold red] ||'))
+Console(style=_A,width=65).print(Panel(f"""[bold red]Maaf,[/][bold white] Anda Harus Menggunakan Perangkat [bold cyan]64-Bit[/][bold white] Dan Python Ver
+si [bold cyan]3.12 Ke Atas[/][bold white] Untuk Menjalankan Script Ini.
+
+Jika Anda Menggunakan Termux, Jalankan Perintah Berikut Di Terminal Anda:
+
+[bold green]$ cd $HOME/SkyHack
+$ chmod +x Run
+$ ./Run[/]
+
+[bold yellow]Peringatan![/][bold white] Script Ini Hanya Untuk Tujuan Pembelajaran. Jan
+gan Gunakan Untuk Hal Negatif! Kami Tidak Bertanggung Jawab Atas Segala Akibat Yang Ditimbulkan Jika Anda Melanggar.""",title='>> [Welcome] <<'))
